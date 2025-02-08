5d156b4be752e43f2409e1e793f61e04a9b7c08b
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2025-02-08 18:51:21 +0400

    add many pointer course

--- src/tests.zig
+++ src/tests.zig
@@ -76,3 +76,109 @@ test "functions" {
 
     try expect(fibbonacci(10) == 55);
 }
+
+test "multi defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    try expect(x == 4.5);
+}
+
+const FileOpenError = error{ AccessDenied, OutOfMemory, FileNotFound };
+
+const AllocationError = error{OutOfMemory};
+
+test "coerce error from a subset to a superset" {
+    const err: FileOpenError = AllocationError.OutOfMemory;
+    try expect(err == FileOpenError.OutOfMemory);
+}
+
+test "error union" {
+    const maybe_error: AllocationError!u16 = 10;
+    const no_error = maybe_error catch 0;
+
+    try expect(@TypeOf(no_error) == u16);
+    try expect(no_error == 10);
+}
+
+fn failingFunction() error{Oops}!void {
+    return error.Oops;
+}
+
+test "returning error" {
+    failingFunction() catch |err| {
+        try expect(err == error.Oops);
+        return;
+    };
+}
+
+var problems: u32 = 98;
+
+fn failFnCounter() error{Oops}!void {
+    errdefer problems += 1;
+    try failingFunction();
+}
+
+test "errdefer" {
+    failFnCounter() catch |err| {
+        try expect(err == error.Oops);
+        try expect(problems == 99);
+        return;
+    };
+}
+
+test "switch statement" {
+    var x: i8 = 10;
+
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+
+        10, 100 => {
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+
+    try expect(x == 1);
+}
+
+test "switch expression" {
+    var x: i8 = 10;
+
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x,
+    };
+}
+
+test "out of bounds" {
+    @setRuntimeSafety(false);
+    const a = [3]u8{ 1, 2, 3 };
+    // std.debug.print("\n", .{});
+    var index: u8 = 255;
+    const b = a[index];
+
+    index = index;
+
+    // std.debug.print("Value: {any}\n", .{b});
+
+    try expect(b == 0);
+}
+
+fn doupleAllManyPointer(buffer: [*]u8, byte_count: usize) void {
+    var i: usize = 0;
+    while (i < byte_count) : (i += 1) buffer[i] *= 2;
+}
+
+test "many items pointer" {
+    var buffer: [100]u8 = [_]u8{1} ** 100;
+    const buffer_ptr: *[100]u8 = &buffer;
+    const many_items_ptr: [*]u8 = buffer_ptr;
+    doupleAllManyPointer(many_items_ptr, buffer.len);
+    for (buffer) |byte| try expect(byte == 2);
+}
